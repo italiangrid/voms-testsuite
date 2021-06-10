559af987f83da7494c38a6db3fdc0b31e1f7f56c
Francesco Giacomini <francesco.giacomini@cnaf.infn.it>
2021-06-10 10:41:05 +0200

    Restore reinstallation of the igi-test-ca rpm

--- compose/assets/scripts/ci-run-testsuite.sh
+++ compose/assets/scripts/ci-run-testsuite.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+# the igi-test-ca was installed during image building, but mounting
+# the /etc/grid-security/certificates volume overwrites the contents
+# of that directory
+sudo yum -y reinstall igi-test-ca
+
 mkdir -p /tmp/reports
 cd /home/test/voms-testsuite
 REPORTS_DIR="/tmp/reports" ./run-testsuite.sh "$@"
