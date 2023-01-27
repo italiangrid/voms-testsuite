cfb2c23f6f3c1dc8665ba29e9809a7c6c60b8473
Federica Agostini <federica.agostini@cnaf.infn.it>
2023-01-27 12:05:52 +0100

    Populate db outside the container

--- compose/assets/scripts/populate-db.sh
+++ compose/assets/scripts/populate-db.sh
@@ -13,8 +13,8 @@ load_db_dump(){
   dump_file=dump-${VO_NAME}.sql
   if [ -f ${SCRIPTS_PREFIX}/${dump_file} ]; then
     echo "Loading dump file ${SCRIPTS_PREFIX}/${dump_file} for VO ${VO_NAME}"
-    mysql -p${MYSQL_ROOT_PASSWORD} -e "create database ${VO_NAME}"
-    cat ${SCRIPTS_PREFIX}/${dump_file} | mysql -p${MYSQL_ROOT_PASSWORD} ${VO_NAME}
+    mysql -h db -u root -p${MYSQL_ROOT_PASSWORD} -e "create database ${VO_NAME}"
+    cat ${SCRIPTS_PREFIX}/${dump_file} | mysql -h db -u root -p${MYSQL_ROOT_PASSWORD} ${VO_NAME}
   else
     echo "${dump_file} not found"
   fi
@@ -25,4 +25,4 @@ for i in $(seq 0 ${VO_COUNT}); do
   load_db_dump $i
 done
 
-mysql -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%'"
+mysql -h db -u root -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%'"
