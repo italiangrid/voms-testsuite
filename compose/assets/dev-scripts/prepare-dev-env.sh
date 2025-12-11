17be4a8700beb3c9f98d73cb5ba62ff8766bc278
Francesco Giacomini <francesco.giacomini@cnaf.infn.it>
2025-12-11 16:06:17 +0100

    Update and simplify dev environment

--- compose/assets/dev-scripts/prepare-dev-env.sh
+++ compose/assets/dev-scripts/prepare-dev-env.sh
@@ -9,23 +9,19 @@ VOMS_HOST=${VOMS_HOST:-voms.test.example}
 DEV_USER=${DEV_USER:-test}
 SCRIPTS="/voms-testsuite/compose/assets/scripts"
 
+docker compose -f compose/docker-compose.dev.yml build --no-cache trust
 docker compose -f compose/docker-compose.dev.yml up -d
 
 docker compose -f compose/docker-compose.dev.yml exec -T db bash /scripts/populate-db.sh
 
 uid=$(id -u)
 gid=$(id -g)
-for service in testsuite-7-stable testsuite-7-beta testsuite-9-beta voms-7-stable voms-7-beta voms-9-beta; do
-  docker compose -f compose/docker-compose.dev.yml exec -T $service sudo groupmod -g ${gid} ${DEV_USER}
-  docker compose -f compose/docker-compose.dev.yml exec -T $service sudo usermod -u ${uid} ${DEV_USER}
-  docker compose -f compose/docker-compose.dev.yml exec -T $service bash -c "sudo ${SCRIPTS}/provide-igi-test-ca.sh"
-done
+docker compose -f compose/docker-compose.dev.yml exec -T testsuite sudo groupmod -g ${gid} ${DEV_USER}
+docker compose -f compose/docker-compose.dev.yml exec -T testsuite sudo usermod -u ${uid} ${DEV_USER}
+docker compose -f compose/docker-compose.dev.yml exec -T voms groupmod -g ${gid} ${DEV_USER}
+docker compose -f compose/docker-compose.dev.yml exec -T voms usermod -u ${uid} ${DEV_USER}
 
-for service in testsuite-7-stable testsuite-7-beta testsuite-9-beta; do
-  docker compose -f compose/docker-compose.dev.yml exec -u root -e VOMS_HOST=${VOMS_HOST} -T $service bash -c "sudo ${SCRIPTS}/setup-testsuite.sh"
-done
+docker compose -f compose/docker-compose.dev.yml exec -u root -e VOMS_HOST=${VOMS_HOST} -T testsuite bash -c "sudo ${SCRIPTS}/setup-testsuite.sh"
 
-for service in voms-7-stable voms-7-beta voms-9-beta; do
-  docker compose -f compose/docker-compose.dev.yml exec -T $service bash ${SCRIPTS}/setup-voms.sh
-  docker compose -f compose/docker-compose.dev.yml exec -T $service bash ${SCRIPTS}/start-voms.sh
-done
+docker compose -f compose/docker-compose.dev.yml exec -T voms bash ${SCRIPTS}/setup-voms.sh
+docker compose -f compose/docker-compose.dev.yml exec -T voms bash ${SCRIPTS}/start-voms.sh
